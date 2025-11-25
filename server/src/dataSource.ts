e9f7802668864b0b4bd3dee98574e19974e23589
Erik <113360820+EConradie@users.noreply.github.com>
2025-11-25 15:29:27 +0200

    Initialize Server
    Installed all dependencies, created folders for the entities as well as the routes. .env setup for database logins.

---
+++ server/src/dataSource.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    synchronize: true,  
+    logging: false,
+    entities: ["src/entities/*.ts"],
+});
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    })
+
+export default AppDataSource
